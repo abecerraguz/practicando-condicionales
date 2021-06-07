9eda69913f0eb938d675894a57b08661d3e40012
abecerraguz <alejandro.becerra@publimetro.cl>
2021-06-06 20:23:09 -0400

    Se guardan los cambios de practicando condicionales

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+var num = 15;
+if (num >= 10) {
+    console.log("el número es mayor que 10");
+    // Salida por consola el número es mayor que 10
+}
+
+var name = "Bruce";
+if (name != "Bruce") {
+    console.log("¿Cuál es tu nombre?");
+} 
+else {
+    console.log("Hola, Bruce!");
+    // Salida por consola de Hola, Bruce!
+}
+
+var num1 = 10;
+var num2 = 15;
+if (num1 <= num2) {
+    num1 = num1 + 10;
+}
+console.log(num1);
+// Salida por consola el num1 es 20
+console.log(num2);
+// Salida por consola el num2 es 15
